1a024a7b7e9328359c13dce20f56ffa1314cc312
harrytaorui <rui.tao@thoughtworks.com>
2020-09-15 18:18:42 +0800

    [taorui] implement function for get&post RsEvent

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,48 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.dto.RsEvent;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+
+  private List<RsEvent> rsList = initRsList();
+
+  private List<RsEvent> initRsList() {
+    List<RsEvent> tempList = new ArrayList<>();
+    tempList.add(new RsEvent("第一条事件",""));
+    tempList.add(new RsEvent("第二条事件",""));
+    tempList.add(new RsEvent("第三条事件",""));
+    return tempList;
+  }
+
+  @GetMapping("/rs/{index}")
+  public RsEvent getRsEvent(@PathVariable int index) {
+    return rsList.get(index - 1);
+  }
+
+  @GetMapping("/rs/list")
+  public List<RsEvent> getRsEventByRange(@RequestParam(required = false) Integer start,
+                                  @RequestParam(required = false) Integer end) {
+    if (start == null || end == null) {
+      return rsList;
+    }
+    return rsList.subList(start - 1, end);
+
+  }
+
+  @PostMapping("/rs/event")
+  public void addRsEvent(@RequestBody String rsEventString) throws JsonProcessingException {
+    ObjectMapper objectMapper = new ObjectMapper();
+    RsEvent rsEvent = objectMapper.readValue(rsEventString,RsEvent.class);
+    rsList.add(rsEvent);
+  }
 }
