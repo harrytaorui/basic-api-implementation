796d60a0354372fbbc27e6d7067e423411bff2cc
harrytaorui <rui.tao@thoughtworks.com>
2020-09-15 14:51:31 +0800

    [taorui] routing function complete

--- src/main/java/com/thoughtworks/rslist/RsListApplication.java
+++ src/main/java/com/thoughtworks/rslist/RsListApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class RsListApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(RsListApplication.class, args);
