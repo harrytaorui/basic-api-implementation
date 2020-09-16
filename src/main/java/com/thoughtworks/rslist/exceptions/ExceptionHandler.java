2959b962b4bb90065877e68144cd3350895574fd
harrytaorui <rui.tao@thoughtworks.com>
2020-09-17 01:46:25 +0800

    [taorui]:Error handler complete

--- src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
@@ -1,20 +1,23 @@
 package com.thoughtworks.rslist.exceptions;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 @ControllerAdvice
 public class ExceptionHandler {
-	@org.springframework.web.bind.annotation.ExceptionHandler({IndexOutOfBoundsException.class,
-			NullPointerException.class,
-			MethodArgumentNotValidException.class})
+	@org.springframework.web.bind.annotation.ExceptionHandler({
+			MyException.class})
 	public ResponseEntity<CommonError> handleIndexOutOfException(Exception ex) {
 		CommonError commonError = new CommonError();
-		if (ex instanceof MethodArgumentNotValidException) {
-			commonError.setError("invalid param");
+
+		if (ex instanceof MyException) {
+			commonError.setError(((MyException) ex).getErrorMsg());
 		}
-		commonError.setError("invalid index");
+		Logger logger = LogManager.getLogger(ExceptionHandler.class);
+		logger.error(commonError.getError());
+
 		return ResponseEntity.badRequest().body(commonError);
 	}
 }
